a6cbfda3936dc2712a3785386d19a7877f4323c5
yinm <yinm.cminor9@gmail.com>
2018-01-06 13:26:21 +0900

    Move properties to constructor

--- 01_modern_practice/todo.ts
+++ 01_modern_practice/todo.ts
@@ -4,13 +4,13 @@ class TodoController {
     this.$routeParams = $routeParams;
     this.$filter = $filter;
     this.store = store;
+
+    this.newtodo = '';
+    this.editedTodo = null;
   }
 }
 
 function TodoCtrl($scope, $routeParams, $filter, store) {
-  $scope.newTodo = '';
-  $scope.editedTodo = null;
-
   $scope.addTodo = function () {};
   $scope.editTodo = function (todo) {};
   $scope.saveEdits = function (todo, event) {};
