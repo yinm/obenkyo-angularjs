57f1dffe921a38315f85d2411f2c248d8cba54bd
yinm <yinm.cminor9@gmail.com>
2018-01-06 13:23:17 +0900

    Add base

---
+++ 01_modern_practice/todo.ts
@@ -0,0 +1,7 @@
+function TodoCtrl($scope, $routeParams, $filter, store) {
+  $scope.newTodo = '';
+  $scope.editedTodo = null;
+  $scope.addTodo = function () {};
+  $scope.editTodo = function (todo) {};
+  $scope.saveEdits = function (todo, event) {};
+}
