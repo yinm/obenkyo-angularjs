1f5beb14b10e22dba655be133f36799722736fc7
yinm <yinm.cminor9@gmail.com>
2018-04-24 13:08:09 +0900

    Check method

--- 01_modern_practice/todo.js
+++ 01_modern_practice/todo.js
@@ -26,6 +26,38 @@ var TodoController = /** @class */ (function () {
             _this.saving = false;
         });
     };
+    TodoController.prototype.editTodo = function (todo) {
+        this.editedTodo = todo;
+        // Clone the original todo to restore it on demand.
+        this.originalTodo = angular.extend({}, todo);
+    };
+    TodoController.prototype.saveEdits = function (todo, event) {
+        var _this = this;
+        // Blur events are automatically triggered after the form submit event,
+        // This does some unfortunate logic handling to prevent saving twice.
+        if (event === 'blur' && this.saveEvent === 'submit') {
+            this.saveEvent = null;
+            return;
+        }
+        this.saveEvent = event;
+        if (this.reverted) {
+            // Todo edits were reverted -- don't save.
+            this.reverted = null;
+            return;
+        }
+        todo.title = todo.title.trim();
+        if (todo.title === this.originalTodo.title) {
+            this.editedTodo = null;
+            return;
+        }
+        store[todo.title ? 'put' : 'delete'](todo)
+            .catch(function () {
+            todo.title = _this.originalTodo.title;
+        })
+            .finally(function () {
+            _this.editedTodo = null;
+        });
+    };
     return TodoController;
 }());
 function TodoCtrl($scope, $routeParams, $filter, store) {
--- 01_modern_practice/todo.ts
+++ 01_modern_practice/todo.ts
@@ -29,6 +29,44 @@ class TodoController {
         this.saving = false
       })
   }
+
+  editTodo(todo) {
+    this.editedTodo = todo
+    // Clone the original todo to restore it on demand.
+    this.originalTodo = angular.extend({}, todo)
+  }
+
+  saveEdits(todo, event) {
+    // Blur events are automatically triggered after the form submit event,
+    // This does some unfortunate logic handling to prevent saving twice.
+    if (event === 'blur' && this.saveEvent === 'submit') {
+      this.saveEvent = null
+      return
+    }
+
+    this.saveEvent = event
+
+    if (this.reverted) {
+      // Todo edits were reverted -- don't save.
+      this.reverted = null
+      return
+    }
+
+    todo.title = todo.title.trim()
+
+    if (todo.title === this.originalTodo.title) {
+      this.editedTodo = null
+      return
+    }
+
+    store[todo.title ? 'put' : 'delete'](todo)
+      .catch(() => {
+        todo.title = this.originalTodo.title
+      })
+      .finally(() => {
+        this.editedTodo = null
+      })
+  }
 }
 
 function TodoCtrl($scope, $routeParams, $filter, store) {
