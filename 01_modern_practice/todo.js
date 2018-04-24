78f3174de4161174527ee872ebd632ef5ed4fd8b
yinm <yinm.cminor9@gmail.com>
2018-04-24 12:49:06 +0900

    Add base

--- 01_modern_practice/todo.js
+++ 01_modern_practice/todo.js
@@ -1,59 +1,7 @@
-var TodoController = (function () {
-    function TodoController($scope, $routeParams, $filter, store) {
-        this.$scope = $scope;
-        this.$routeParams = $routeParams;
-        this.$filter = $filter;
-        this.store = store;
-        this.newTodo = '';
-        this.editedTodo = null;
-        TodoController.$inject = ['$scope', '$routeParams', '$filter', 'store'];
-    }
-    TodoController.prototype.addTodo = function () {
-        var _this = this;
-        var newTodo = {
-            title: this.newTodo.trim(),
-            completed: false,
-        };
-        if (!newTodo.title) {
-            return;
-        }
-        this.saving = true;
-        store
-            .insert(newTodo)
-            .then(function () {
-            _this.newTodo = '';
-        })
-            .finally(function () {
-            _this.saving = false;
-        });
-    };
-    TodoController.prototype.editTodo = function (todo) {
-        this.editedTodo = todo;
-        this.originalTodo = angular.extend({}, todo);
-    };
-    TodoController.prototype.saveEdits = function (todo, event) {
-        var _this = this;
-        if (event === 'blur' && this.saveEvent === 'submit') {
-            this.saveEvent = null;
-            return;
-        }
-        this.saveEvent = event;
-        if (this.reverted) {
-            this.reverted = null;
-            return;
-        }
-        todo.title = todo.title.trim();
-        if (todo.title === this.originalTodo.title) {
-            this.editedTodo = null;
-            return;
-        }
-        store[todo.title ? 'put' : 'delete'](todo)
-            .then(function () { }, function () {
-            todo.title = _this.originalTodo.title;
-        })
-            .finally(function () {
-            _this.editedTodo = null;
-        });
-    };
-    return TodoController;
-}());
+function TodoCtrl($scope, $routeParams, $filter, store) {
+    $scope.newTodo = '';
+    $scope.editedTodo = null;
+    $scope.addTodo = function () { };
+    $scope.editTodo = function (todo) { };
+    $scope.saveEdits = function (todo, event) { };
+}
--- 01_modern_practice/todo.ts
+++ 01_modern_practice/todo.ts
@@ -1,68 +1,8 @@
-class TodoController {
-  constructor($scope, $routeParams, $filter, store) {
-    this.$scope = $scope;
-    this.$routeParams = $routeParams;
-    this.$filter = $filter;
-    this.store = store;
+function TodoCtrl($scope, $routeParams, $filter, store) {
+  $scope.newTodo = ''
+  $scope.editedTodo = null
 
-    this.newTodo = '';
-    this.editedTodo = null;
-
-    TodoController.$inject = ['$scope', '$routeParams', '$filter', 'store'];
-  }
-
-  addTodo() {
-    const newTodo = {
-      title: this.newTodo.trim(),
-      completed: false,
-    };
-
-    if (!newTodo.title) {
-      return;
-    }
-
-    this.saving = true;
-    store
-      .insert(newTodo)
-      .then(() => {
-        this.newTodo = '';
-      })
-      .finally(() => {
-        this.saving = false;
-      });
-  }
-
-  editTodo(todo) {
-    this.editedTodo = todo;
-    this.originalTodo = angular.extend({}, todo);
-  }
-
-  saveEdits(todo, event) {
-    if (event === 'blur' && this.saveEvent === 'submit') {
-      this.saveEvent = null;
-      return;
-    }
-
-    this.saveEvent = event;
-
-    if (this.reverted) {
-      this.reverted = null;
-      return;
-    }
-
-    todo.title = todo.title.trim();
-
-    if (todo.title === this.originalTodo.title) {
-      this.editedTodo = null;
-      return;
-    }
-
-    store[todo.title ? 'put' : 'delete'](todo)
-      .then(() => {}, () => {
-        todo.title = this.originalTodo.title;
-      })
-      .finally(() => {
-        this.editedTodo = null;
-      });
-  }
+  $scope.addTodo = function() {}
+  $scope.editTodo = function(todo) {}
+  $scope.saveEdits = function(todo, event) {}
 }
